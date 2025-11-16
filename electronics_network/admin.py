b896c775eb09c5c6d252c1013677baedc249c0de
elenasyurtukova <SyurtukovaES@hotmail.com>
2025-11-16 21:33:25 +0900

    add clear_debt

--- electronics_network/admin.py
+++ electronics_network/admin.py
@@ -23,6 +23,19 @@ class ElectronicsNetworkAdmin(admin.ModelAdmin):
                     "contacts_house_number", "supplier_link", "level", "get_products", "debt_to_supplier", "created_at")
     list_filter = ("contacts_city", )
     search_fields = ("name", )
+    actions = ['clear_debt']
 
     def get_products(self, instance):
         return [product.name for product in instance.products.all()]
+
+    def clear_debt(self, request, queryset):
+        """функция обнуления задолженности перед поставщиком в объекте звена сети"""
+        for network in queryset:
+            supplier = network.supplier
+            if supplier:
+                supplier.debt_to_supplier -= network.debt_to_supplier
+                supplier.save()
+        queryset.update(debt_to_supplier=0)
+        self.message_user(request, "Задолженность успешно очищена.")
+
+    clear_debt.short_description="Очистить задолженность у выбранных объектов"
